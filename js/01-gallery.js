815e158384902cfc3b01408ff615250f20b31ce1
EvgenBatt <evgenmgid@gmail.com>
2023-04-30 00:11:49 +0300

    Complete tasks 1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const imageCard = galleryItems.reduce((acc, { preview, original, description }) => {
+  return (acc += `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`);
+}, "");
+
+gallery.insertAdjacentHTML("afterbegin", imageCard);
+
+let instance;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const imageSrc = event.target.dataset.source;
+    instance = basicLightbox.create(`<img src="${imageSrc}" width="1280">`);
+    instance.show();
+  }
+});
+
+const closeInstance = (event) => {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+};
+
+document.addEventListener("keydown", closeInstance);
+
+instance.element().addEventListener("click", () => {
+  document.removeEventListener("keydown", onEscapePress);
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const imageCard = galleryItems.reduce((acc, { preview, original, description }) => {
+  return (acc += `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`);
+}, "");
+
+gallery.insertAdjacentHTML("afterbegin", imageCard);
+
+const galleryLightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionsPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
